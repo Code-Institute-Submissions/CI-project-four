e4df41d75a53dd7f6979bfa3d1de8d92ea332795
markwilde33 <markwilde29@gmail.com>
2018-09-14 17:14:35 +0100

    Added a route/function to get the recipes collection and render the collection on the recipes.html page

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'flask_cookbook'
+app.config["MONGO_URI"] = 'mongodb://admin:cookbook33@ds233320.mlab.com:33320/flask_cookbook'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello Clarice'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html",
+                           recipes=mongo.db.recipes.find())
+
+
+# @app.route('/')
+# def hello():
+#     return 'Hello Clarice'
 
 
 if __name__ == '__main__':
